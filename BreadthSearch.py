606859fda77ffdb8882393390174548c91d2e0af
brunovsc <bruno.vieira@zup.com.br>
2018-04-03 19:58:52 -0300

    Working on breadth first search algorithm

--- BreadthSearch.py
+++ BreadthSearch.py
@@ -1,2 +1,27 @@
 from Classes import Robot
+from Classes import State
 
+statusA = input("Situation for space A: ")
+statusB = input("Situation for space B: ")
+
+robotPosition = input("Robot position: ")
+
+state = State([statusA, statusB], Robot(robotPosition))
+
+state.print_info()
+
+toExplore = []
+toExplore.append(state)
+
+while(len(toExplore)):
+    
+    stateToExplore = toExplore.pop()
+    if(stateToExplore.is_final_state()):
+        stateToExplore.print_info()
+        break
+    
+    for action in state.robot.actions:
+        newState = state.apply_action(action)
+        toExplore.append(newState)
+    
+print("404 - Solution not found")
